b828c1227ab316a00bc37f04d4f9805c89667804
Guoxiaojie <1176787592@qq.com>
2023-05-25 19:55:17 +0800

    修改图片无法显示bug

--- src/page/HomePage.vue
+++ src/page/HomePage.vue
@@ -21,7 +21,7 @@ const switchLanguage = () => {
 }
 
 const goToNotFound = useGoToNotFound();
-const avatarSrc = ref("public/R.gif"); // GIF 图片的路径
+const avatarSrc = ref("R.gif"); // GIF 图片的路径
 
 </script>
 
@@ -55,7 +55,7 @@ const avatarSrc = ref("public/R.gif"); // GIF 图片的路径
 <style scoped>
 .container {  
   overflow: hidden;
-  background-image: url('public/image.jpg');
+  background-image: url('image.jpg');
   background-size: 100% 100%;
   background-repeat: no-repeat;
   background-position: center center;
@@ -85,7 +85,7 @@ const avatarSrc = ref("public/R.gif"); // GIF 图片的路径
 }
 @font-face {
   font-family: 'Pacifico'; /* 自定义字体的名称 */
-  src: url('../public/font/Pacifico.ttf'); /* 字体文件的路径 */
+  src: url('font/Pacifico.ttf'); /* 字体文件的路径 */
 }
 .title-wrapper h1 {
   display: inline;
